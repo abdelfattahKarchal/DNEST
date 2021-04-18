ea5431af866bfee11115641af4d7509aa42e1f66
Abdelfattah <abdelfattah59@gmail.com>
2021-04-18 22:50:35 +0000

    fix headers

--- resources/views/front/partials/headers/header-principal.blade.php
+++ resources/views/front/partials/headers/header-principal.blade.php
@@ -1,70 +1,93 @@
 <div class="header-bottom_area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-2 col-md-4 col-sm-4">
-                        <div class="header-logo">
-                            <a href="{{ route('index') }}">
-                                <img src="{{ asset('front/assets/images/logo/logo-header.png') }}" alt="The DNest jewelery" class="m-1">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-7 d-none d-lg-block position-static d-lg-flex justify-content-center">
-                        <div class="main-menu_area">
-                            <nav>
-                                <ul>
-                                    <li class="dropdown-holder"><a href="{{ route('index') }}">Home</a></li>
-                             <li class="dropdown-holder"><a href="javascript:void(0)">Collection</a>
-                                 <ul class="hm-dropdown">
-                                     @foreach ($collections as $collection)
-                                         @if ($collection->active == 1)
-                                             <li>
-                                                 <a
-                                                     href="{{ url('collections/' . $collection->id . '/products') }}">{{ $collection->name }}</a>
-                                             </li>
-                                         @endif
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-4 col-sm-4">
+                <div class="header-logo">
+                    <a href="{{ route('index') }}">
+                        <img src="{{ asset('front/assets/images/logo/logo-header.png') }}" alt="The DNest jewelery"
+                            class="m-1">
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block position-static d-lg-flex justify-content-center">
+                <div class="main-menu_area">
+                    <nav>
+                        <ul>
+                            <li class="dropdown-holder"><a href="{{ route('index') }}">Home</a></li>
+                            <li class="dropdown-holder"><a href="javascript:void(0)">Collection</a>
+                                <ul class="hm-dropdown">
+                                    @foreach ($collections as $collection)
+                                        @if ($collection->active == 1)
+                                            <li>
+                                                <a
+                                                    href="{{ url('collections/' . $collection->id . '/products') }}">{{ $collection->name }}</a>
+                                            </li>
+                                        @endif
 
-                                     @endforeach($collections as $collection)
+                                    @endforeach($collections as $collection)
 
-                                 </ul>
-                             </li>
-                             <li><a href=" {{ route('blogs.index') }} ">Blog</a></li>
-                             <li><a href=" {{ route('aboutUs') }} ">About Us</a></li>
-                             <li><a href="{{ route('contact') }}">Contact</a></li>
                                 </ul>
-                            </nav>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-8 col-sm-8">
-                        <div class="header-right_area">
-                            <ul>
-                                <li>
-                                    <a href="wishlist.html" class="wishlist-btn">
-                                        <i class="ion-person"></i>
-                                        {{ Auth::user()->name ?? null}}
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('orders.index') }}" class="minicart-btn toolbar-btn">
-                                        <i class="ion-bag">
-                                            <sup>
-                                                <span class="badge badge-light card-counter">{{ count(session()->get('productsCardSession') ?? []) }}</span>
-                                            </sup>
-                                        </i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#mobileMenu" class="mobile-menu_btn toolbar-btn color--white d-lg-none d-block">
-                                        <i class="ion-navicon"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                            </li>
+                            <li><a href=" {{ route('blogs.index') }} ">Blog</a></li>
+                            <li><a href=" {{ route('aboutUs') }} ">About Us</a></li>
+                            <li><a href="{{ route('contact') }}">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-8 col-sm-8">
+                <div class="header-right_area main-menu_area">
+                    <nav>
+                        <ul>
+                            <li class="dropdown-holder">
+                                <a href="javascript:void(0)" class="wishlist-btn">
+                                    <i class="ion-person" style="font-size: 1.5rem;"></i>
+                                    {{ Auth::user()->name ?? null }}
+                                </a>
+                                <ul class="hm-dropdown">
+                                    @if (!Auth::check())
+                                        <li><a href="{{ route('register.form') }}">Register</a></li>
+                                        <li><a href="{{ route('login.form') }}">Login</a></li>
+                                    @else
+                                        <li><a href="{{ route('login.form') }}">My Profile</a></li>
+                                        <li>
+                                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                                Sign Out</a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                class="d-none">
+                                                @csrf
+                                            </form>
+                                        </li>
+                                    @endif
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="{{ route('orders.index') }}">
+                                    <i class="ion-bag" style="font-size: 1.5rem;">
+                                        <sup>
+                                            <span
+                                                class="badge badge-light card-counter">{{ count(session()->get('productsCardSession') ?? []) }}</span>
+                                        </sup>
+                                    </i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#mobileMenu"
+                                    class="mobile-menu_btn toolbar-btn color--white d-lg-none d-block">
+                                    <i class="ion-navicon" style="font-size: 1.5rem;"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+
                 </div>
             </div>
         </div>
+    </div>
+</div>
 
- {{-- <div class="header-bottom_area">
+{{-- <div class="header-bottom_area">
      <div class="container">
          <div class="row">
             <div class="col-lg-2 col-md-4 col-sm-4">
@@ -139,4 +162,4 @@
          </div>
      </div>
  </div> --}}
- <!-- Fin Menu -->
+<!-- Fin Menu -->
--- resources/views/front/partials/headers/header-scroll.blade.php
+++ resources/views/front/partials/headers/header-scroll.blade.php
@@ -4,144 +4,84 @@
                 <div class="row">
                     <div class="col-lg-2 col-md-4 col-sm-4">
                         <div class="header-logo">
-                            <a href="index.html">
-                                <img src="assets/images/menu/logo/2.png" alt="Hiraola's Header Logo">
+                            <a href="{{ route('index') }}">
+                                <img src="{{ asset('front/assets/images/logo/logo-header.png') }}" alt="The DNest jewelery"
+                                    class="m-1">
                             </a>
                         </div>
                     </div>
-                    <div class="col-lg-7 d-none d-lg-block position-static">
+                    <div class="col-lg-7 d-none d-lg-block position-static d-lg-flex justify-content-center">
                         <div class="main-menu_area">
                             <nav>
                                 <ul>
-                                    <li class="dropdown-holder"><a href="index.html">Home</a>
+                                    <li class="dropdown-holder"><a href="{{ route('index') }}">Home</a></li>
+                                    <li class="dropdown-holder"><a href="javascript:void(0)">Collection</a>
                                         <ul class="hm-dropdown">
-                                            <li><a href="index.html">Home One</a></li>
-                                            <li><a href="index-2.html">Home Two</a></li>
-                                            <li class="active"><a href="index-3.html">Home Three</a></li>
+                                            @foreach ($collections as $collection)
+                                                @if ($collection->active == 1)
+                                                    <li>
+                                                        <a
+                                                            href="{{ url('collections/' . $collection->id . '/products') }}">{{ $collection->name }}</a>
+                                                    </li>
+                                                @endif
+        
+                                            @endforeach($collections as $collection)
+        
                                         </ul>
                                     </li>
-                                    <li class="megamenu-holder"><a href="shop-left-sidebar.html">Shop</a>
-                                        <ul class="hm-megamenu">
-                                            <li><span class="megamenu-title">Shop Page Layout</span>
-                                                <ul>
-                                                    <li><a href="shop-3-column.html">Grid Fullwidth</a></li>
-                                                    <li><a href="shop-left-sidebar.html">Left Sidebar</a></li>
-                                                    <li><a href="shop-right-sidebar.html">Right Sidebar</a></li>
-                                                    <li><a href="shop-list-fullwidth.html">List Fullwidth</a></li>
-                                                    <li><a href="shop-list-left-sidebar.html">List Left Sidebar</a></li>
-                                                    <li><a href="shop-list-right-sidebar.html">List Right Sidebar</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li><span class="megamenu-title">Single Product Style</span>
-                                                <ul>
-                                                    <li><a href="single-product-gallery-left.html">Gallery Left</a></li>
-                                                    <li><a href="single-product-gallery-right.html">Gallery Right</a>
-                                                    </li>
-                                                    <li><a href="single-product-tab-style-left.html">Tab Style Left</a>
-                                                    </li>
-                                                    <li><a href="single-product-tab-style-right.html">Tab Style
-                                                        Right</a>
-                                                    </li>
-                                                    <li><a href="single-product-sticky-left.html">Sticky Left</a></li>
-                                                    <li><a href="single-product-sticky-right.html">Sticky Right</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><span class="megamenu-title">Single Product Type</span>
-                                                <ul>
-                                                    <li><a href="single-product.html">Single Product</a></li>
-                                                    <li><a href="single-product-sale.html">Single Product Sale</a></li>
-                                                    <li><a href="single-product-group.html">Single Product Group</a>
-                                                    </li>
-                                                    <li><a href="single-product-variable.html">Single Product
-                                                        Variable</a>
-                                                    </li>
-                                                    <li><a href="single-product-affiliate.html">Single Product
-                                                        Affiliate</a>
-                                                    </li>
-                                                    <li><a href="single-product-slider.html">Single Product Slider</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li class="menu-item_img"></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="blog-left-sidebar.html">Blog</a>
-                                        <ul class="hm-dropdown">
-                                            <li><a href="blog-left-sidebar.html">Grid View</a>
-                                                <ul class="hm-dropdown hm-sub_dropdown">
-                                                    <li><a href="blog-2-column.html">Column Two</a></li>
-                                                    <li><a href="blog-3-column.html">Column Three</a></li>
-                                                    <li><a href="blog-left-sidebar.html">Left Sidebar</a></li>
-                                                    <li><a href="blog-right-sidebar.html">Right Sidebar</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="blog-list-left-sidebar.html">List View</a>
-                                                <ul class="hm-dropdown hm-sub_dropdown">
-                                                    <li><a href="blog-list-fullwidth.html">List Fullwidth</a></li>
-                                                    <li><a href="blog-list-left-sidebar.html">List Left Sidebar</a></li>
-                                                    <li><a href="blog-list-right-sidebar.html">List Right Sidebar</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="blog-details-left-sidebar.html">Blog Details</a>
-                                                <ul class="hm-dropdown hm-sub_dropdown">
-                                                    <li><a href="blog-details-left-sidebar.html">Left Sidebar</a></li>
-                                                    <li><a href="blog-details-right-sidebar.html">Right Sidebar</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="blog-gallery-format.html">Blog Format</a>
-                                                <ul class="hm-dropdown hm-sub_dropdown">
-                                                    <li><a href="blog-gallery-format.html">Gallery Format</a></li>
-                                                    <li><a href="blog-audio-format.html">Audio Format</a></li>
-                                                    <li><a href="blog-video-format.html">Video Format</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="index.html">Pages</a>
-                                        <ul class="hm-dropdown">
-                                            <li><a href="my-account.html">My Account</a></li>
-                                            <li><a href="login-register.html">Login | Register</a></li>
-                                            <li><a href="wishlist.html">Wishlist</a></li>
-                                            <li><a href="cart.html">Cart</a></li>
-                                            <li><a href="checkout.html">Checkout</a></li>
-                                            <li><a href="compare.html">Compare</a></li>
-                                            <li><a href="faq.html">FAQ</a></li>
-                                            <li><a href="404.html">404 Error</a></li>
-                                            <li><a href="coming-soon_page.html">Comming Soon</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="about-us.html">About Us</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
+                                    <li><a href=" {{ route('blogs.index') }} ">Blog</a></li>
+                                    <li><a href=" {{ route('aboutUs') }} ">About Us</a></li>
+                                    <li><a href="{{ route('contact') }}">Contact</a></li>
                                 </ul>
                             </nav>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-8 col-sm-8">
-                        <div class="header-right_area">
-                            <ul>
-                                <li>
-                                    <a href="wishlist.html" class="wishlist-btn">
-                                        <i class="ion-android-favorite-outline"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#searchBar" class="search-btn toolbar-btn">
-                                        <i class="ion-ios-search-strong"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#mobileMenu" class="mobile-menu_btn toolbar-btn color--white d-lg-none d-block">
-                                        <i class="ion-navicon"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#miniCart" class="minicart-btn toolbar-btn">
-                                        <i class="ion-bag"></i>
-                                    </a>
-                                </li>
-                            </ul>
+                        <div class="header-right_area main-menu_area">
+                            <nav>
+                                <ul>
+                                    <li class="dropdown-holder">
+                                        <a href="javascript:void(0)" class="wishlist-btn">
+                                            <i class="ion-person" style="font-size: 1.5rem;"></i>
+                                            {{ Auth::user()->name ?? null }}
+                                        </a>
+                                        <ul class="hm-dropdown">
+                                            @if (!Auth::check())
+                                                <li><a href="{{ route('register.form') }}">Register</a></li>
+                                                <li><a href="{{ route('login.form') }}">Login</a></li>
+                                            @else
+                                                <li><a href="{{ route('login.form') }}">My Profile</a></li>
+                                                <li>
+                                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                document.getElementById('logout-form').submit();">
+                                                        Sign Out</a>
+                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                        class="d-none">
+                                                        @csrf
+                                                    </form>
+                                                </li>
+                                            @endif
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="{{ route('orders.index') }}">
+                                            <i class="ion-bag" style="font-size: 1.5rem;">
+                                                <sup>
+                                                    <span
+                                                        class="badge badge-light card-counter">{{ count(session()->get('productsCardSession') ?? []) }}</span>
+                                                </sup>
+                                            </i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#mobileMenu"
+                                            class="mobile-menu_btn toolbar-btn color--white d-lg-none d-block">
+                                            <i class="ion-navicon" style="font-size: 1.5rem;"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+        
                         </div>
                     </div>
                 </div>
